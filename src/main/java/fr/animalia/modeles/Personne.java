8841b0bb134ee3a8844891fa5f26b45b2d49f65f
Philippe CORTEZ <philippe.cortez.pro@gmail.com>
2022-06-21 17:41:41 +0200

    Ajout classe Personne (sans lien)

---
+++ src/main/java/fr/animalia/modeles/Personne.java
@@ -0,0 +1,68 @@
+package fr.animalia.modeles;
+
+import lombok.*;
+
+import java.time.LocalDate;
+
+/**
+ * Une personne est reconnue par ses informations personnelles (nom, prénom, date naissance)
+ * Une personne est enregistré lorsqu'elle adopte un animal du refuge, elle devient ainsi son maître
+ * @author Philippe CORTEZ
+ */
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+
+@Getter
+public class Personne
+{
+    /** L'identifiant d'une personne en tant que maître*/
+    private long numMaitre;
+
+    /** Le nom de la personne */
+    private String nom;
+
+    /** Le prénom de la personne */
+    private String prenom;
+
+    /** Date de naissance de la personne */
+    private LocalDate dateNaissance;
+
+    /** L'adresse de la personne */
+    @Setter
+    private String adresse;
+
+
+
+    /** Redéfinition de equals */
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (!(o instanceof Personne)) return false;
+
+        Personne personne = (Personne) o;
+
+        return numMaitre == personne.numMaitre;
+    }
+
+    /** Redéfinition de hashCode */
+    @Override
+    public int hashCode()
+    {
+        return (int) (numMaitre ^ (numMaitre >>> 32));
+    }
+
+    /** Redéfinition de toString */
+    @Override
+    public String toString()
+    {
+        return "Personne{" +
+                "numMaitre=" + numMaitre +
+                ", nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                ", dateNaissance=" + dateNaissance +
+                ", adresse='" + adresse + '\'' +
+                '}';
+    }
+
+}
