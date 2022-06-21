2c1a80299e2ed1ae45a7535ba9c11c6dee043cc3
Philippe CORTEZ <philippe.cortez.pro@gmail.com>
2022-06-21 14:36:21 +0200

    Squelette base du projet + modelisation UML base

---
+++ src/main/java/fr/animalia/App.java
@@ -0,0 +1,13 @@
+package fr.animalia;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
