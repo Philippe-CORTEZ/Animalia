f2de65fd534270ac486402946be3f37ad771c6d9
Philippe CORTEZ <philippe.cortez.pro@gmail.com>
2022-06-22 15:28:17 +0200

    Ajout DAO Pathologie

---
+++ src/main/java/fr/animalia/bdds/daos/PathologieDAO.java
@@ -0,0 +1,11 @@
+package fr.animalia.bdds.daos;
+
+import fr.animalia.modeles.Pathologie;
+
+/**
+ * La DAO qui gère les objets de type Pathologie
+ * @author Philippe CORTEZ
+ */
+public class PathologieDAO extends DAOGenerique<Pathologie>
+{
+}
