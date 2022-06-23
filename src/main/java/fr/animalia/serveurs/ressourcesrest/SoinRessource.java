a7a469f8da0c15836e5a94a96f9ba38f9a4a9619
Philippe CORTEZ <philippe.cortez.pro@gmail.com>
2022-06-23 11:51:56 +0200

    Ajout recherche par prix Soin ressource rest

--- src/main/java/fr/animalia/serveurs/ressourcesrest/SoinRessource.java
+++ src/main/java/fr/animalia/serveurs/ressourcesrest/SoinRessource.java
@@ -2,7 +2,13 @@ package fr.animalia.serveurs.ressourcesrest;
 
 import fr.animalia.bdds.daos.SoinDAO;
 import fr.animalia.modeles.Soin;
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
+
+import java.util.List;
 
 /**
  * Gère les ressources de la classe Soin
@@ -11,10 +17,30 @@ import jakarta.ws.rs.Path;
 @Path("soins")
 public class SoinRessource extends RessourceGenerique<Soin>
 {
+    /** Utilise une SoinDAO en type déclaré pour les actions propres à cette ressource */
+    private final SoinDAO soinDAO;
+
+
     /** Utilise la DAO de Soin pour les méthodes de la classe mère */
     public SoinRessource()
     {
         this.dao = new SoinDAO();
+        this.soinDAO = new SoinDAO();
     }
 
+
+    /**
+     * Récupère les soins comprit dans une fourchette de prix
+     * @param prixBas prix le plus bas (borne début)
+     * @param prixHaut prix le plus haut (borne de fin)
+     * @return la liste des soins dans la fourchette de prix
+     */
+    @GET
+    @Path("prix")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Soin> recupererParPrix(@QueryParam("prixBas") float prixBas, @QueryParam("prixHaut") float prixHaut)
+    {
+        return soinDAO.rechercherParPrix(prixBas, prixHaut);
+    }
+    
 }
