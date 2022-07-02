4fcc96c86aeece0f9dc2a0151a6b17fb24079526
Philippe CORTEZ <philippe.cortez.pro@gmail.com>
2022-07-02 12:55:54 +0200

    Ajout constructeur sans args prive classe DonneIHM

--- src/main/java/fr/animalia/controleurs/DonneeIHM.java
+++ src/main/java/fr/animalia/controleurs/DonneeIHM.java
@@ -1,13 +1,16 @@
 package fr.animalia.controleurs;
 
 import fr.animalia.modeles.Animal;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 /**
  * Classe qui stock les données nécessaires entre plusieurs fenêtres
  * @author Philippe CORTEZ
  */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class DonneeIHM
 {
     /** L'animal sélectionné par l'utilisateur */
