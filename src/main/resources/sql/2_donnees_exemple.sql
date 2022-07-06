03dc6a0bd09168853026035d42fc504504807076
Philippe CORTEZ <philippe.cortez.pro@gmail.com>
2022-07-06 19:03:54 +0200

    Retrait des id auto genere

--- src/main/resources/sql/2_donnees_exemple.sql
+++ src/main/resources/sql/2_donnees_exemple.sql
@@ -27,66 +27,66 @@ Sleepy est très joueuse, elle n''attend qu''un foyer pour l''aimer', true, 0);
 
 
 -- REFUGE --
-INSERT INTO public.refuge (id, nom, adresse, description) VALUES (1, 'ANIMALIA-ROUEN', '325-RUE-ALBERT-CAMUS-76-ROUEN', 'Animalerie spécialisée dans les pensionnaires domestiques type chien, chat');
-INSERT INTO public.refuge (id, nom, adresse, description) VALUES (2, 'ANIMALIA-LA-GARDE', 'AVENUE-UNIVERSITE-83-LA-GARDE', 'Spécialiste des NAC');
-INSERT INTO public.refuge (id, nom, adresse, description) VALUES (3, 'ANIMALIA-PARIS-CENTRE', 'AVENUE-BERCY-75-PARIS-CENTRE', 'Proche du stade de bercy');
-INSERT INTO public.refuge (id, nom, adresse, description) VALUES (4, 'ANIMALIA-MARSEILLE', 'RUE-DU-PRADO-13-MARSEILLE', 'Proche du prado à marseille, accueille les animaux de compagnies standard (chats, chiens, oiseaux)');
-INSERT INTO public.refuge (id, nom, adresse, description) VALUES (5, 'ANIMALIA-VALLAURIS', '2323-CHEMIN-DE-SAINT-BERNARD-06-VALLAURIS', 'A coté de l''entreprise Avisto');
-INSERT INTO public.refuge (id, nom, adresse, description) VALUES (6, 'ANIMALIA-RENNES', '4785-PLACE-LOIC-35-RENNE', 'Capital de la cybersécurité, votre futur animal l''est aussi');
+INSERT INTO public.refuge (nom, adresse, description) VALUES ('ANIMALIA-ROUEN', '325-RUE-ALBERT-CAMUS-76-ROUEN', 'Animalerie spécialisée dans les pensionnaires domestiques type chien, chat');
+INSERT INTO public.refuge (nom, adresse, description) VALUES ('ANIMALIA-LA-GARDE', 'AVENUE-UNIVERSITE-83-LA-GARDE', 'Spécialiste des NAC');
+INSERT INTO public.refuge (nom, adresse, description) VALUES ('ANIMALIA-PARIS-CENTRE', 'AVENUE-BERCY-75-PARIS-CENTRE', 'Proche du stade de bercy');
+INSERT INTO public.refuge (nom, adresse, description) VALUES ('ANIMALIA-MARSEILLE', 'RUE-DU-PRADO-13-MARSEILLE', 'Proche du prado à marseille, accueille les animaux de compagnies standard (chats, chiens, oiseaux)');
+INSERT INTO public.refuge (nom, adresse, description) VALUES ('ANIMALIA-VALLAURIS', '2323-CHEMIN-DE-SAINT-BERNARD-06-VALLAURIS', 'A coté de l''entreprise Avisto');
+INSERT INTO public.refuge (nom, adresse, description) VALUES ('ANIMALIA-RENNES', '4785-PLACE-LOIC-35-RENNE', 'Capital de la cybersécurité, votre futur animal l''est aussi');
 
 
 
 -- SOIN --
-INSERT INTO public.soin (id, nom, description, prix, pourcentage_charge) VALUES (3, 'VERMIFUGE', 'Suppression des vers dans l''estomac', 30, 0.75);
-INSERT INTO public.soin (id, nom, description, prix, pourcentage_charge) VALUES (4, 'ANTI-PUCE', 'Traitement anti puces dans le bain', 20, 0.4);
-INSERT INTO public.soin (id, nom, description, prix, pourcentage_charge) VALUES (5, 'VACCIN-RAGE', 'Vaccin contre la rage', 40, 0.5);
-INSERT INTO public.soin (id, nom, description, prix, pourcentage_charge) VALUES (6, 'VACCIN-PARVOVIROSE', 'Vaccin contre la parvovirose', 35, 0.3);
-INSERT INTO public.soin (id, nom, description, prix, pourcentage_charge) VALUES (7, 'VACCIN-GRIPPE', 'Vaccin contr la grippe', 30, 0.1);
+INSERT INTO public.soin (nom, description, prix, pourcentage_charge) VALUES ('VERMIFUGE', 'Suppression des vers dans l''estomac', 30, 0.75);
+INSERT INTO public.soin (nom, description, prix, pourcentage_charge) VALUES ('ANTI-PUCE', 'Traitement anti puces dans le bain', 20, 0.4);
+INSERT INTO public.soin (nom, description, prix, pourcentage_charge) VALUES ('VACCIN-RAGE', 'Vaccin contre la rage', 40, 0.5);
+INSERT INTO public.soin (nom, description, prix, pourcentage_charge) VALUES ('VACCIN-PARVOVIROSE', 'Vaccin contre la parvovirose', 35, 0.3);
+INSERT INTO public.soin (nom, description, prix, pourcentage_charge) VALUES ('VACCIN-GRIPPE', 'Vaccin contr la grippe', 30, 0.1);
 
 
 
 -- PATHOLOGIE --
-INSERT INTO public.pathologie (id, nom, description) VALUES (1, 'HYDROCEPHALE', 'Eau dans la boite crânienne');
-INSERT INTO public.pathologie (id, nom, description) VALUES (2, 'PARVOVIROSE', 'Gasto-entérite sévère voire mortelle');
-INSERT INTO public.pathologie (id, nom, description) VALUES (3, 'LEISHMANIOSE', 'Maladie cutanée');
-INSERT INTO public.pathologie (id, nom, description) VALUES (4, 'CYSTITE', 'Affection urinaire');
-INSERT INTO public.pathologie (id, nom, description) VALUES (5, 'FIV-FELINE', 'Le virus de l''immunodéficience féline (FIV) est une grave infection virale chez le chat.
+INSERT INTO public.pathologie (nom, description) VALUES ('HYDROCEPHALE', 'Eau dans la boite crânienne');
+INSERT INTO public.pathologie (nom, description) VALUES ('PARVOVIROSE', 'Gasto-entérite sévère voire mortelle');
+INSERT INTO public.pathologie (nom, description) VALUES ('LEISHMANIOSE', 'Maladie cutanée');
+INSERT INTO public.pathologie (nom, description) VALUES ('CYSTITE', 'Affection urinaire');
+INSERT INTO public.pathologie (nom, description) VALUES ('FIV-FELINE', 'Le virus de l''immunodéficience féline (FIV) est une grave infection virale chez le chat.
 Même s''il est similaire au VIH (sida) humain, le FIV est spécifique à une espèce, ce qui signifie qu''il peut être uniquement transmis entre chats et non pas aux humains ou autres animaux.');
 
 
 
 -- INFORMATION SEJOUR --
-INSERT INTO public.information_sejour (id, num_puce_animal, id_refuge, date_debut_sejour, date_fin_sejour, motif_fin_sejour, refuge_actuel) VALUES (1, 741852963741852, 1, '2019-07-12', null, null, true);
-INSERT INTO public.information_sejour (id, num_puce_animal, id_refuge, date_debut_sejour, date_fin_sejour, motif_fin_sejour, refuge_actuel) VALUES (2, 789456123078945, 5, '2020-02-07', null, null, true);
-INSERT INTO public.information_sejour (id, num_puce_animal, id_refuge, date_debut_sejour, date_fin_sejour, motif_fin_sejour, refuge_actuel) VALUES (3, 951753825641025, 3, '2021-11-26', null, null, true);
-INSERT INTO public.information_sejour (id, num_puce_animal, id_refuge, date_debut_sejour, date_fin_sejour, motif_fin_sejour, refuge_actuel) VALUES (4, 341256987410256, 2, '2021-09-17', null, null, true);
-INSERT INTO public.information_sejour (id, num_puce_animal, id_refuge, date_debut_sejour, date_fin_sejour, motif_fin_sejour, refuge_actuel) VALUES (5, 415263985201467, 6, '2018-10-26', null, null, true);
-INSERT INTO public.information_sejour (id, num_puce_animal, id_refuge, date_debut_sejour, date_fin_sejour, motif_fin_sejour, refuge_actuel) VALUES (6, 123005664789541, 4, '2020-04-10', null, null, true);
-INSERT INTO public.information_sejour (id, num_puce_animal, id_refuge, date_debut_sejour, date_fin_sejour, motif_fin_sejour, refuge_actuel) VALUES (7, 333214569985201, 2, '2019-07-13', null, null, true);
-INSERT INTO public.information_sejour (id, num_puce_animal, id_refuge, date_debut_sejour, date_fin_sejour, motif_fin_sejour, refuge_actuel) VALUES (8, 895100224563510, 1, '2015-12-30', null, null, true);
-INSERT INTO public.information_sejour (id, num_puce_animal, id_refuge, date_debut_sejour, date_fin_sejour, motif_fin_sejour, refuge_actuel) VALUES (9, 414523654100238, 4, '2018-10-24', null, null, true);
-INSERT INTO public.information_sejour (id, num_puce_animal, id_refuge, date_debut_sejour, date_fin_sejour, motif_fin_sejour, refuge_actuel) VALUES (10, 655210047899663, 6, '2016-08-18', null, null, true);
+INSERT INTO public.information_sejour (num_puce_animal, id_refuge, date_debut_sejour, date_fin_sejour, motif_fin_sejour, refuge_actuel) VALUES (741852963741852, 1, '2019-07-12', null, null, true);
+INSERT INTO public.information_sejour (num_puce_animal, id_refuge, date_debut_sejour, date_fin_sejour, motif_fin_sejour, refuge_actuel) VALUES (789456123078945, 5, '2020-02-07', null, null, true);
+INSERT INTO public.information_sejour (num_puce_animal, id_refuge, date_debut_sejour, date_fin_sejour, motif_fin_sejour, refuge_actuel) VALUES (951753825641025, 3, '2021-11-26', null, null, true);
+INSERT INTO public.information_sejour (num_puce_animal, id_refuge, date_debut_sejour, date_fin_sejour, motif_fin_sejour, refuge_actuel) VALUES (341256987410256, 2, '2021-09-17', null, null, true);
+INSERT INTO public.information_sejour (num_puce_animal, id_refuge, date_debut_sejour, date_fin_sejour, motif_fin_sejour, refuge_actuel) VALUES (415263985201467, 6, '2018-10-26', null, null, true);
+INSERT INTO public.information_sejour (num_puce_animal, id_refuge, date_debut_sejour, date_fin_sejour, motif_fin_sejour, refuge_actuel) VALUES (123005664789541, 4, '2020-04-10', null, null, true);
+INSERT INTO public.information_sejour (num_puce_animal, id_refuge, date_debut_sejour, date_fin_sejour, motif_fin_sejour, refuge_actuel) VALUES (333214569985201, 2, '2019-07-13', null, null, true);
+INSERT INTO public.information_sejour (num_puce_animal, id_refuge, date_debut_sejour, date_fin_sejour, motif_fin_sejour, refuge_actuel) VALUES (895100224563510, 1, '2015-12-30', null, null, true);
+INSERT INTO public.information_sejour (num_puce_animal, id_refuge, date_debut_sejour, date_fin_sejour, motif_fin_sejour, refuge_actuel) VALUES (414523654100238, 4, '2018-10-24', null, null, true);
+INSERT INTO public.information_sejour (num_puce_animal, id_refuge, date_debut_sejour, date_fin_sejour, motif_fin_sejour, refuge_actuel) VALUES (655210047899663, 6, '2016-08-18', null, null, true);
 
 
 
 
 -- SOINS_ANIMAUX --
+INSERT INTO public.soins_animaux (num_puce_animal, id_soin) VALUES (741852963741852, 1);
+INSERT INTO public.soins_animaux (num_puce_animal, id_soin) VALUES (741852963741852, 2);
 INSERT INTO public.soins_animaux (num_puce_animal, id_soin) VALUES (741852963741852, 3);
-INSERT INTO public.soins_animaux (num_puce_animal, id_soin) VALUES (741852963741852, 4);
-INSERT INTO public.soins_animaux (num_puce_animal, id_soin) VALUES (741852963741852, 5);
+INSERT INTO public.soins_animaux (num_puce_animal, id_soin) VALUES (789456123078945, 3);
 INSERT INTO public.soins_animaux (num_puce_animal, id_soin) VALUES (789456123078945, 5);
-INSERT INTO public.soins_animaux (num_puce_animal, id_soin) VALUES (789456123078945, 7);
-INSERT INTO public.soins_animaux (num_puce_animal, id_soin) VALUES (951753825641025, 4);
-INSERT INTO public.soins_animaux (num_puce_animal, id_soin) VALUES (951753825641025, 5);
+INSERT INTO public.soins_animaux (num_puce_animal, id_soin) VALUES (951753825641025, 2);
+INSERT INTO public.soins_animaux (num_puce_animal, id_soin) VALUES (951753825641025, 3);
+INSERT INTO public.soins_animaux (num_puce_animal, id_soin) VALUES (415263985201467, 3);
 INSERT INTO public.soins_animaux (num_puce_animal, id_soin) VALUES (415263985201467, 5);
-INSERT INTO public.soins_animaux (num_puce_animal, id_soin) VALUES (415263985201467, 7);
-INSERT INTO public.soins_animaux (num_puce_animal, id_soin) VALUES (333214569985201, 5);
+INSERT INTO public.soins_animaux (num_puce_animal, id_soin) VALUES (333214569985201, 3);
+INSERT INTO public.soins_animaux (num_puce_animal, id_soin) VALUES (895100224563510, 1);
+INSERT INTO public.soins_animaux (num_puce_animal, id_soin) VALUES (895100224563510, 2);
 INSERT INTO public.soins_animaux (num_puce_animal, id_soin) VALUES (895100224563510, 3);
-INSERT INTO public.soins_animaux (num_puce_animal, id_soin) VALUES (895100224563510, 4);
-INSERT INTO public.soins_animaux (num_puce_animal, id_soin) VALUES (895100224563510, 5);
-INSERT INTO public.soins_animaux (num_puce_animal, id_soin) VALUES (655210047899663, 4);
+INSERT INTO public.soins_animaux (num_puce_animal, id_soin) VALUES (655210047899663, 2);
+INSERT INTO public.soins_animaux (num_puce_animal, id_soin) VALUES (655210047899663, 3);
 INSERT INTO public.soins_animaux (num_puce_animal, id_soin) VALUES (655210047899663, 5);
-INSERT INTO public.soins_animaux (num_puce_animal, id_soin) VALUES (655210047899663, 7);
 
 
 
